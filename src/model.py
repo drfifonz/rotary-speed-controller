2e9e9d7927414402a98984a19ae9c8a19096e5c2
drfifonz <fp.patyk@gmail.com>
2022-12-02 01:24:31 +0100

    add LQR regulation for linear DC motor model

--- src/model.py
+++ src/model.py
@@ -1,34 +1,95 @@
 import numpy as np
+from numpy.linalg import inv
 from scipy.integrate.odepack import odeint
+from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
-L = 0.0014    # motor inductance
-R = 0.5       #motor resistance
-J = 0.003     #inertia momentum
-b = 0.0002    #friction
-k_ME = 0.1    #factor converting mechanical energy into electrical 
-k_EM = 0.1    #factor converting electrical energy into mechanical
+L = 0.0014  # motor inductance
+R = 0.5  # motor resistance
+J = 0.003  # inertia momentum
+b = 0.0002  # friction
+k_ME = 0.1  # factor converting mechanical energy into electrical
+k_EM = 0.1  # factor converting electrical energy into mechanical
 
-def force(t):
-    return 24 
 
-A = np.array([[-R/L,-k_ME/L],[k_EM/J,-b/J]])
-B = np.array([[1/L],[0]])
-C = np.array([0,1])
-def model(x,t):
-    dx = A @ np.array([[x[0]],[x[1]]]) +B * force(t)
-    y = C @ np.array([[x[0]], [x[1]]])
+speed_set = 1000
+omega_set = (speed_set * 2 * np.pi) / 60
 
-    return [dx[0,0],dx[1,0]]
-#definnig time 
+
+Q = np.eye(2)
+S = np.eye(2)
+R = np.array([[1]])
+
+# definnig time
 t = np.linspace(0, 10, 2000)
-res = odeint(model,[0,0],t)
+t_ricc = t[::-1]  # reverset time for riccati equation
+t_ricc = np.linspace(5, 0, 1000)
 
-plt.figure()
-plt.title("DC motor simulation")
-plt.grid()
-plt.subplot(1, 2, 1)
-plt.plot(t, res[:, 0])
-plt.subplot(1, 2, 2)
-plt.plot(t, res[:, 1])
+X0 = np.array([omega_set, 0])
+U0 = 0
+
+
+A = np.array([[-R / L, -k_ME / L], [k_EM / J, -b / J]])
+B = np.array([[1 / L], [0]])
+C = np.array([0, 1])
+
+
+def riccati(p, t):
+    dP = np.reshape(p, [2, 2])
+    ddP = -(dP @ A - dP @ B @ inv(R) @ B.T @ dP + A.T @ dP + Q)
+
+    return np.ndarray.tolist(np.reshape(ddP, [1, -1])[0])
+
+
+res = odeint(riccati, [1, 0, 0, 1], t_ricc)
+P11 = interp1d(t_ricc, res[:, 0], fill_value="extrapolate")
+P12 = interp1d(t_ricc, res[:, 1], fill_value="extrapolate")
+P21 = interp1d(t_ricc, res[:, 2], fill_value="extrapolate")
+P22 = interp1d(t_ricc, res[:, 3], fill_value="extrapolate")
+
+
+def model(x, t):
+    x = np.array([x]).T
+
+    u = 20
+
+    dx = A @ x + B * u
+    return [dx[0, 0], dx[1, 0]]
+
+
+def model_ricc(x, t):
+    x = np.array([x]).T
+
+    P = np.array([[P11(t), P12(t)], [P21(t), P22(t)]])
+
+    K = inv(R) @ B.T @ P
+
+    nx = x - X0
+    nu = -K @ nx + U0
+
+    dx = A @ nx + B * nu
+
+    return np.ndarray.tolist(dx.T[0])
+
+
+res_model = odeint(model, [0, 0], t)
+ricc_model = odeint(model_ricc, [0, 0], t, full_output=0)
+
+
+fig, axs = plt.subplots(2, 2)
+fig.suptitle("DC motor simulation")
+fig.set_size_inches(20, 10)
+axs[0, 0].set_title("current")
+axs[0, 0].plot(t, res_model[:, 0])
+
+axs[0, 1].set_title("rotational speed")
+axs[0, 1].plot(t, res_model[:, 1])
+
+axs[1, 0].set_title("Ricc current")
+axs[1, 0].plot(t, ricc_model[:, 0])
+
+axs[1, 1].set_title("Ricc rotational speed")
+axs[1, 1].plot(t, ricc_model[:, 1])
+
+
 plt.show()
