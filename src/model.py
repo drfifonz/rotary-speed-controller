263245650fe1e91ef85287e35bacf0aecb8f8e75
drfifonz <fp.patyk@gmail.com>
2022-12-04 23:32:40 +0100

    fix resistance and R matrix overriding issue

--- src/model.py
+++ src/model.py
@@ -5,7 +5,7 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 L = 0.0014  # motor inductance
-R = 0.5  # motor resistance
+Res = 0.5  # motor resistance
 J = 0.003  # inertia momentum
 b = 0.0002  # friction
 k_ME = 0.1  # factor converting mechanical energy into electrical
@@ -23,13 +23,13 @@ R = np.array([[1]])
 # definnig time
 t = np.linspace(0, 10, 2000)
 t_ricc = t[::-1]  # reverset time for riccati equation
-t_ricc = np.linspace(5, 0, 1000)
+# t_ricc = np.linspace(10, 0, 1000)
 
 X0 = np.array([omega_set, 0])
 U0 = 0
 
 
-A = np.array([[-R / L, -k_ME / L], [k_EM / J, -b / J]])
+A = np.array([[-Res / L, -k_ME / L], [k_EM / J, -b / J]])
 B = np.array([[1 / L], [0]])
 C = np.array([0, 1])
 
